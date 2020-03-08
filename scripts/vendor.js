cf00fd6d1abc0d84cd8435f9e7095b82f28a39b9
Luís Miguel <luism@marzeelabs.org>
2020-03-08 23:07:58 +0000

    Optimize fonts and css build

--- scripts/vendor.js
+++ scripts/vendor.js
@@ -3,13 +3,13 @@
 const fs = require('fs-extra');
 
 // Include FontAwesome Webfonts for proper icon rendering
-fs.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts', err => err && console.error(err));
+fs.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/lib/webfonts', err => err && console.error(err));
 
 // Copy Netlify CMS from dependency directory
-fs.copy('node_modules/netlify-cms/dist/netlify-cms.js', 'public/js/netlify-cms.js', err => err && console.error(err));
+fs.copy('node_modules/netlify-cms/dist/netlify-cms.js', 'public/lib/netlify-cms.js', err => err && console.error(err));
 
 // Tiny Slider
-fs.copy('node_modules/tiny-slider/dist/tiny-slider.css', 'public/css/tiny-slider.css', err => err && console.error(err));
+fs.copy('node_modules/tiny-slider/dist/tiny-slider.css', 'public/lib/tiny-slider.css', err => err && console.error(err));
 
 // Lightbox
-fs.copy('node_modules/lightbox2/dist', 'public/lightbox', err => err && console.error(err));
+fs.copy('node_modules/lightbox2/dist', 'public/lib/lightbox', err => err && console.error(err));
