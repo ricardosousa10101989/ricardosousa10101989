0fef455811f53b0957dcb9ef8bf89e1c8bc10908
Luís Miguel <luism@marzeelabs.org>
2020-03-11 23:53:18 +0000

    Fix logo animation and breakpoints

--- src/components/highlight-nav.js
+++ src/components/highlight-nav.js
@@ -2,15 +2,26 @@
   const navbar = document.querySelector('.navbar');
   let scrollTimeout = null;
 
+  const handleScroll = () => {
+    const what = jQuery(window).scrollTop() ? 'add' : 'remove';
+    navbar.classList[what]('navbar--scrolled');
+    document.body.classList[what]('scrolled');
+    scrollTimeout = null;
+  };
+
   // Highlight the top nav as scrolling occurs
   window.addEventListener('scroll', () => {
     if (!scrollTimeout) {
-      scrollTimeout = setTimeout(() => {
-        const what = jQuery(window).scrollTop() ? 'add' : 'remove';
-        navbar.classList[what]('navbar--scrolled');
-        document.body.classList[what]('scrolled');
-        scrollTimeout = null;
-      }, 150);
+      scrollTimeout = setTimeout(handleScroll, 150);
     }
   });
+
+  // When refreshing an already scrolled page, the logo will appear floating
+  // in the middle of the page. To fix this, we immediately apply the scrolled
+  // class.
+  handleScroll();
+
+  setTimeout(() => {
+    document.body.classList.add('transitions-enabled');
+  }, 100);
 })();
