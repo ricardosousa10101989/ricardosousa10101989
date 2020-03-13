9a0885e93e72e7cb9b1d0f949fca6beb349c6248
Luís Miguel <luism@marzeelabs.org>
2020-03-13 23:14:12 +0000

    Logo animation tweaks

--- src/components/highlight-nav.js
+++ src/components/highlight-nav.js
@@ -1,11 +1,29 @@
 (() => {
   const navbar = document.querySelector('.navbar');
+  const header = document.querySelector('.header');
   let scrollTimeout = null;
 
   const handleScroll = () => {
-    const what = jQuery(window).scrollTop() ? 'add' : 'remove';
-    navbar.classList[what]('navbar--scrolled');
-    document.body.classList[what]('scrolled');
+    const scrollTop = jQuery(window).scrollTop();
+
+    // If more than half the header is gone, the transitions speed up, to
+    // avoid the logo staying visible over the rest of the content.
+    if (scrollTop > header.clientHeight / 2) {
+      document.body.classList.add('header-gone');
+    }
+    else {
+      document.body.classList.remove('header-gone');
+    }
+
+    if (scrollTop) {
+      navbar.classList.add('navbar--scrolled');
+      document.body.classList.add('scrolled');
+    }
+    else {
+      navbar.classList.remove('navbar--scrolled');
+      document.body.classList.remove('scrolled');
+    }
+
     scrollTimeout = null;
   };
 
