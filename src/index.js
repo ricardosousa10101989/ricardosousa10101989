339772fd2187ee600989442ffd5bde324e1799e5
Luís Miguel <luism@marzeelabs.org>
2020-03-15 11:59:11 +0000

    Implement GDPR banner

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './components/contact';
 import './components/gallery';
+import './components/gdpr';
 import './components/highlight-nav';
 import './components/highlight-section';
 import './components/mobile-menu';
