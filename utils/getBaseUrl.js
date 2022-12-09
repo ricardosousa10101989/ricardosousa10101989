8981e2c9559300940a5137112ad02f4b1d0a3892
Luís Miguel <luism@marzeelabs.org>
2022-12-09 13:07:32 +0000

    Bugfix absolute urls

--- utils/getBaseUrl.js
+++ utils/getBaseUrl.js
@@ -3,6 +3,11 @@ const getBaseUrl = () => {
     return window.location.origin;
   }
 
+  // Netlify Deployment via Github Actions
+  if (process.env.PREFIX) {
+    return `https://${process.env.PREFIX}--${process.env.SITE_NAME}.netlify.app`;
+  }
+
   // This will only work for the main domain, it will not work for deploy previews because
   // DEPLOY_PRIME_URL is not passed there; netlify-plugin-inline-functions-env does not seem to
   // work either.
