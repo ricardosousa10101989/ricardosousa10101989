7ff506ce36557247eb951c3dd225564517465b64
Luís Miguel <luism@marzeelabs.org>
2022-12-11 17:53:04 +0000

    Show loading indicator on lightbox

--- components/Lightbox/Lightbox.jsx
+++ components/Lightbox/Lightbox.jsx
@@ -1,5 +1,5 @@
 import {
-  useCallback, useEffect, useMemo, useState,
+  useCallback, useEffect, useMemo, useRef, useState,
 } from 'react';
 import classnames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,28 +14,45 @@ const Lightbox = () => {
 
   const [ lightbox, setLightbox ] = useLightbox();
   const [ last, setLast ] = useState(null);
+  const [ loaded, setLoaded ] = useState(false);
 
   const close = useCallback(() => setLightbox(null), [ setLightbox ]);
+  const timer = useRef();
 
   const clickableContainer = useMemo(() => prepareClickableContainer(close), [ close ]);
 
   // Prevent scrolling the page in the background while lightbox is visible
   useEffect(() => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+      timer.current = null;
+    }
+
     if (lightbox) {
       document.body.style.overflow = 'hidden';
 
-      setLast(lightbox);
+      if (last !== lightbox) {
+        setLast(lightbox);
+        setLoaded(false);
+      }
+    }
+    else {
+      timer.current = setTimeout(() => {
+        setLast(null);
+        setLoaded(false);
+      }, 400);
     }
 
     return () => {
       document.body.style.overflow = '';
     };
-  }, [ lightbox ]);
+  }, [ last, lightbox ]);
 
   return firstRender && (
     <div
       className={ classnames('lightbox', {
         'lightbox--visible': lightbox,
+        'lightbox--loaded': loaded,
       }) }
     >
       <div
@@ -55,13 +72,25 @@ const Lightbox = () => {
       </button>
 
       <div className="lightbox__content">
-        <img
-          alt={ last?.title || 'hidden' }
-          className="lightbox__img"
-          sizes="100vw"
-          src={ last?.src }
-          srcSet={ last?.srcset }
-        />
+        <div className="lightbox__img-wrapper">
+          { !!lightbox && !loaded && (
+            <FontAwesomeIcon
+              className="lightbox__img lightbox__img--spinner"
+              icon="fa-solid fa-spinner"
+            />
+          ) }
+
+          <img
+            alt={ last?.title || 'hidden' }
+            className={ classnames('lightbox__img', 'lightbox__img--main', {
+              'lightbox__img--loaded': loaded,
+            }) }
+            onLoad={ () => setLoaded(true) }
+            sizes="100vw"
+            src={ last?.src }
+            srcSet={ last?.srcset }
+          />
+        </div>
 
         <div className="lightbox__details">
           { last?.title }
