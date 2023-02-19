70ffdbf6aeaeb1235ade16e4b6989cc12e8cd8d8
Luís Miguel <luis.miguel@realfevr.com>
2023-02-19 16:35:22 +0000

    Tweak icons

--- components/Icon/Forest/Forest.jsx
+++ components/Icon/Forest/Forest.jsx
@@ -1,11 +1,18 @@
 import classnames from 'classnames';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import IconFlower from 'components/Icon/Flower';
 import IconTree from 'components/Icon/Tree';
 
 const IconForest = ({ className }) => (
   <div className={ classnames('icon-forest', className) }>
     <IconTree />
     <IconTree />
+    <IconFlower />
+    { /*
+    <IconFlower />
+    <FontAwesomeIcon icon="fa-solid fa-seedling" />
+    */ }
   </div>
 );
 
